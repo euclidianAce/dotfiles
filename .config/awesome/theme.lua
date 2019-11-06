d08cfef8f3973d5fd69bc0b320de15752b2f9f7d
Corey Williamson <nerd1001001@gmail.com>
2019-11-06 17:54:53 -0600

    Small theme changes

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -28,7 +28,7 @@ theme.fg_focus      = theme.bg_normal
 theme.fg_urgent     = theme.bg_normal
 theme.fg_minimize   = theme.bg_normal
 
-theme.useless_gap   = 0
+theme.useless_gap   = 3
 theme.border_width  = dpi(1)
 theme.border_normal = xrdb.color0
 theme.border_focus  = theme.bg_focus

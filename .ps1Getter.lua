48609e912119349f4e85acc046517b87f3fbd0d5
= <nerd1001001@gmail.com>
2019-12-04 00:55:57 -0600

    ps1 script: refactored chunk object and moved to its own file

--- .ps1Getter.lua
+++ .ps1Getter.lua
@@ -1,10 +1,11 @@
 #!/bin/env lua
 
 -- TODO:
--- 	- move some stuff to other files so they dont have to be reloaded everytime the script is run
 -- 	- find a nice looking way of truncating things when terminal is too thin
---	- less verbose variable names for chunks
 
+local chunk = require "chunk"
+local box = chunk.box
+local color = chunk.color
 -- Functions to call bash functions and get varaibles
 function bashExec(expr)
 	local f = io.popen(expr)
@@ -19,93 +20,11 @@ function bashEchoInto(expr)
 	return bashExec("echo "..expr)
 end
 
-local ansiColors = {
-	black	= "90m",
-	red	= "91m",
-	green	= "92m",
-	yellow	= "93m",
-	blue	= "94m",
-	magenta	= "95m",
-	cyan	= "96m",
-	white	= "97m",
-
-	darkBlack	= "30m",
-	darkRed		= "31m",
-	darkGreen	= "32m",
-	darkYellow	= "33m",
-	darkBlue	= "34m",
-	darkMagenta	= "35m",
-	darkCyan	= "36m",
-	darkWhite	= "37m"
-}
-
--- but a string between non-printing characters so bash doesnt get confused when calculating length
-local escStr =  table.concat{
-	"\\[",		 -- surrounding brackets so bash knows not to count length
-	string.char(27), -- escape, aka \e, \033, etc.
-	"[%s\\]"	 -- command to be filled with str:format
-}
-local function esc(str)
-	return escStr:format(str)
-end
-
--- chunk object
--- 	basically a string except when you want the length to be different
--- 	since the box drawing characters count as length 3 we want to accurately calculate the length of the string
--- 	also escaped characters for colors and bold should count as length 0
---
--- 	this is an object that lets you define the length of the string you give it
-local chunk = {}
-local chunkMt 
-
-local function newChunk(str, len)
-	if not len then len = #str end
-	return setmetatable({str=str, len=len}, chunkMt)
-end
-
-chunkMt = {
-	__metatable = "chunk",
-	__concat = function(a, b)
-		return newChunk( a.str..b.str, a.len+b.len )
-	end,
-	__index = {
-		rep = function(self, num)
-			return newChunk( self.str:rep(num), self.len*num )
-		end,
-		color = function(self, colorName)
-			return chunk.esc[colorName] .. self .. chunk.esc.reset
-		end
-	}
-}
-
-
-chunk.box = {}
-chunk.box.vline 	= newChunk("│",1)
-chunk.box.hline 	= newChunk("─",1)
-chunk.box.tlcorner 	= newChunk("┌",1)
-chunk.box.blcorner 	= newChunk("└",1)
-chunk.box.trcorner	= newChunk("┐",1)
-chunk.box.brcorner	= newChunk("┘",1)
-chunk.box.tleft 	= newChunk("┤",1)
-chunk.box.tright 	= newChunk("├",1)
-chunk.box.tdown		= newChunk("┬",1)
-chunk.box.tup		= newChunk("┴",1)
-chunk.box.cross		= newChunk("┼",1)
-
-chunk.esc = {}
-for i, v in pairs(ansiColors) do
-	chunk.esc[i] = newChunk(esc(v),0)
-end
-chunk.esc.bold 	= newChunk(esc("1m"),0)
-chunk.esc.reset = newChunk(esc("0m"),0)
-chunk.newl 	= newChunk('\n',0)
-
-
 -- User and terminal info
 
 local columns 	= tonumber( bashEchoInto("$(stty size)"):gsub("(%d+)%s+(%d+)", "%2"), nil )
 
-local user 	= newChunk(
+local user 	= chunk.new(
 			bashEchoInto("$USER") .. "@" .. bashExec("hostname")
 		)
 
@@ -113,79 +32,84 @@ local workDir	= bashEchoInto("$DIRSTACK") or ""
 if workDir == "" then
 	workDir = bashExec("pwd"):gsub("/home/"..bashEchoInto("$USER"), "~")
 end
-workDir	= newChunk( workDir..(" "):rep(10-#workDir) ) -- Make the working directory at least 10 chars long 
+workDir	= chunk.new( workDir..(" "):rep(10-#workDir) ) -- Make the working directory at least 10 chars long 
       
 local gitNoBranchStr = "* none"
 local gitBranch = bashExec("git branch 2> /dev/null | grep \\*")
-      gitBranch = (gitBranch and newChunk(gitBranch)) 
-      		or newChunk(gitNoBranchStr)
+      gitBranch = (gitBranch and chunk.new(gitBranch)) 
+      		or chunk.new(gitNoBranchStr)
 
-local time 	= newChunk(os.date("%X"))
+local time 	= chunk.new(os.date("%X"))
 
 -- Helper functions
-function concat(...)
-	local rChunk = newChunk("")
-	for _, v in ipairs{...} do
-		rChunk = rChunk..v
-	end
-	return rChunk
-end
 
-local lineColor = "darkCyan"
-local timeColor = "darkWhite"
-local userColor = "red"
-local workDirColor = "blue"
-local gitColor = (gitBranch.str ~= gitNoBranchStr and "green") or "yellow"
+local lineColor = "cyan"
+local timeColor = "white"
+local userColor = "lightRed"
+local workDirColor = "lightBlue"
+local gitColor = (gitBranch.str ~= gitNoBranchStr and "lightGreen") or "lightYellow"
+
 -- PS1
 
+--[[
 if 10+time.len+user.len+workDir.len+gitBranch.len > columns then -- compact mode
-	workDir = newChunk(bashEchoInto("$DIRSTACK")):color(workDirColor)
+	workDir = chunk.new(bashEchoInto("$DIRSTACK")):color(workDirColor)
 	local ps1 = concat(
-		workDir, newChunk(" $ "):color("magenta") 
+		workDir, chunk.new(" $ "):color("lightMagenta") 
 	)
 
 	io.write(ps1.str)
 	return
 end
+]]
 
--- first line
-local firstLine = concat(
-	chunk.esc[lineColor], 
-	-- Initial Spaces
-	newChunk((" "):rep(3)),
-	-- Corner and T above time
-	chunk.box.tlcorner, chunk.box.hline:rep(time.len), chunk.box.tdown,
-	-- Ts above username
-	chunk.box.hline:rep(user.len), chunk.box.tdown,
-	-- Ts above working directory
-	chunk.box.hline:rep(workDir.len), chunk.box.tdown,
-	-- T and corner above git branch
-	chunk.box.hline:rep(gitBranch.len), chunk.box.trcorner
-)
-local secondLine = concat(	
-	chunk.box.tright, chunk.box.hline:rep(2), 
-	chunk.box.tleft, 
-	time:color(timeColor), 		chunk.box.vline:color(lineColor),
-	user:color(userColor), 		chunk.box.vline:color(lineColor), 
-	workDir:color(workDirColor), 	chunk.box.vline:color(lineColor), 
-	gitBranch:color(gitColor), 	chunk.box.tright:color(lineColor)
-)
-local columnsLeft = columns - secondLine.len - 1
-secondLine = concat(secondLine,
-	chunk.esc[lineColor], chunk.box.hline:rep(columnsLeft), chunk.box.tleft
-)
-local thirdLine = concat(
-	newChunk((" "):rep(3)), chunk.box.blcorner, chunk.box.hline:rep(time.len), chunk.box.tup, 
-	chunk.box.hline:rep(user.len),
+local ps1 = {
+	chunk.concat{
+	-- line 1, the hats to the info
 
-	chunk.box.tup, chunk.box.hline:rep(workDir.len),
-	chunk.box.tup, chunk.box.hline:rep(gitBranch.len), chunk.box.brcorner
-)
+	--[[ Set the lineColor ]] 	color[lineColor],
+	--[[ Initial Spaces ]] 		chunk.new(" "):rep(3),
+	--[[ Corner and T above time]] 	box.corner.topLeft, box.line.horizontal:rep(time.len), box.t.down,
+	--[[ Ts above user@host ]] 	box.line.horizontal:rep(user.len), box.t.down,
+	--[[ Ts above directory ]] 	box.line.horizontal:rep(workDir.len), box.t.down,
+	--[[ corner above git branch ]] box.line.horizontal:rep(gitBranch.len), box.corner.topRight,
+					chunk.newl,
+	},
+	chunk.concat{
+		-- line 2, the info and the separators for it
+		
+		-- initial stuffs
+		box.corner.topLeft, box.line.horizontal:rep(2), box.t.left, chunk.reset,
+		--   INFO				SEPARATOR
+		time:color(timeColor), 		box.line.vertical:color(lineColor),
+		user:color(userColor),		box.line.vertical:color(lineColor),
+		workDir:color(workDirColor),	box.line.vertical:color(lineColor),
+		gitBranch:color(gitColor),	color[lineColor], box.t.right,
+	},
 
-local fourthLine = newChunk("$ "):color("darkMagenta")
+	-- line 3, the bottom bits
+	chunk.concat{
+	--[[ Set the lineColor ]] 	color[lineColor], box.line.vertical,
+	--[[ Initial Spaces ]] 		chunk.new(" "):rep(2),
+	--[[ Corner and T above time]] 	box.corner.bottomLeft, box.line.horizontal:rep(time.len), box.t.up,
+	--[[ Ts above user@host ]] 	box.line.horizontal:rep(user.len), box.t.up,
+	--[[ Ts above directory ]] 	box.line.horizontal:rep(workDir.len), box.t.up,
+	--[[ corner above git branch ]] box.line.horizontal:rep(gitBranch.len), box.corner.bottomRight,
+					chunk.reset, chunk.newl,
 
+	},
 
+	-- line 4, the $
+	chunk.concat{
+		color[lineColor],
+		box.corner.bottomLeft, box.t.left, 
+		chunk.new("$ "):color("magenta"), chunk.reset,
+	},
+}
 
-local ps1 = concat(firstLine, chunk.newl,  secondLine, chunk.newl, thirdLine, chunk.newl, fourthLine)
+local columnsLeft = columns - ps1[2].len - 1
+table.insert(ps1, 3, box.line.horizontal:rep(columnsLeft) .. box.t.left .. chunk.newl)
+
+ps1 = chunk.concat(ps1)
 -- "return" the string to bash
 io.write(ps1.str)
---
+++ chunk.lua
@@ -0,0 +1,105 @@
+#!/bin/env lua
+
+local table = require "table"
+local string = require "string"
+
+local escapeString = table.concat{"\\[", string.char(27), "[%s\\]"}
+local function esc(str)
+	return escapeString:format(str)
+end
+local resetString = esc("0m")
+
+local ANSIColors = {
+	black		= "30m",
+	red		= "31m",
+	green		= "32m",
+	yellow		= "33m",
+	blue		= "34m",
+	magenta		= "35m",
+	cyan		= "36m",
+	white		= "37m",
+	
+	lightBlack	= "90m",
+	lightRed	= "91m",
+	lightGreen	= "92m",
+	lightYellow	= "93m",
+	lightBlue	= "94m",
+	lightMagenta	= "95m",
+	lightCyan	= "96m",
+	lightWhite	= "97m",
+}
+
+
+local chunk = {}
+local __chunk, box, color
+
+
+function chunk.new(str, len)
+	if not len then len = #str end
+	return setmetatable({str=str, len=len}, __chunk)
+end
+
+
+__chunk = {
+	__metatable = "chunk",
+	__concat = function(c1, c2)
+		return chunk.new( c1.str .. c2.str, c1.len + c2.len )
+	end,
+	__tostring = function(self)
+		return self.str
+	end,
+	__index = {
+		rep = function(self, num)
+			return chunk.new( self.str:rep(num), self.len * num )
+		end,
+		color = function(self, colorName)
+			return chunk.new(esc(ANSIColors[colorName]), 0) .. self .. chunk.new(resetString,0)
+		end
+	}
+}
+
+
+color = {}
+for name, val in pairs(ANSIColors) do
+	color[name] = chunk.new( esc(ANSIColors[name]), 0 )
+end
+
+
+box = {
+	line = {
+		vertical 	= chunk.new("│",1),
+		horizontal 	= chunk.new("─",1),
+		cross 		= chunk.new("┼",1),
+	},
+	corner = {
+		topLeft 	= chunk.new("┌",1),
+		topRight 	= chunk.new("┐",1),
+		bottomLeft 	= chunk.new("└",1),
+		bottomRight 	= chunk.new("┘",1),
+	},
+	t = {
+		up 		= chunk.new("┴",1),
+		down 		= chunk.new("┬",1),
+		left 		= chunk.new("┤",1),
+		right 		= chunk.new("├",1),
+	},
+}
+
+function chunk.concat(tab)
+	local strTable = {}
+	local newLen = 0
+	for _, v in ipairs(tab) do
+		table.insert(strTable, v.str)
+		newLen = newLen + v.len
+	end
+	return chunk.new(
+		table.concat(strTable),
+		newLen
+	)
+end
+
+chunk.newl = chunk.new("\n", 0)
+chunk.reset = chunk.new(resetString, 0)
+chunk.color = color
+chunk.box = box
+return chunk

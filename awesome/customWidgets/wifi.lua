252c7a3c34e734bd91f9c5f16e54d1d269790626
Corey Williamson <nerd1001001@gmail.com>
2020-01-14 17:53:55 -0600

    Wifi: fix when str:find is nil+added default text

--- awesome/customWidgets/wifi.lua
+++ awesome/customWidgets/wifi.lua
@@ -5,27 +5,32 @@ local beautiful = require "beautiful"
 
 local interface = "wlp2s0"
 local cmd = ("wpa_cli -i %s list_networks | grep CURRENT"):format(interface)
+local badText = " Not Connected "
+local text = wibox.widget.textbox(badText)
 
-
-local icon_path = "../icons/"
-local wifi_good_icon = icon_path .. "wifi.svg"
-local wifi_bad_icon = icon_path .. "wifi-off.svg"
-
-local wifi_icon_widget = wibox.widget.imagebox()
 
 local wifi = wibox.widget {
 	layout = wibox.layout.align.horizontal,
-	{widget = wifi_icon_widget},
-	{widget = awful.widget.watch(("sh -c '%s'"):format(cmd), 3, function(widget, stdout)
+	{widget = awful.widget.watch(("sh -c '%s'"):format(cmd), 1, function(widget, stdout)
+		
 		local start = stdout:find("\t")
 		if not start then
-			widget:set_text(" Not Connected ")
+			widget:set_text(badText)
+			return
 		else
 			start = start + 1
 		end
-		local finish = stdout:find("\t", start)-1
+
+		local finish = stdout:find("\t", start)
+		if not finish then
+			widget:set_text(badText)
+			return
+		else
+			finish = finish - 1
+		end
+
 		widget:set_text(stdout:sub(start, finish))
-	end)}
+	end, text)}
 }
 
 

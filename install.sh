59272721207ef641811ebf4b837a59f7b4473e59
Corey Williamson <nerd1001001@gmail.com>
2020-03-29 04:24:50 -0500

    Added .gitconfig

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@
 # .bashrc
 # .vimrc
 # .xinitrc
+# .gitconfig
 # .Xresources
 
 (ln -f ~/.config/.bashrc ~/.bashrc && echo .bashrc hard linked) || echo .bashrc hard link failed
 (ln -f ~/.config/.vimrc ~/.vimrc && echo .vimrc hard linked) || echo .vimrc hard link failed
 (ln -f ~/.config/.xinitrc ~/.xinitrc && echo .xinitrc hard linked) || echo .xinitrc hard link failed
 (ln -f ~/.config/.Xresources ~/.Xresources && echo .Xresources hard linked) || echo .Xresources hard link failed
+(ln -f ~/.config/.gitconfig ~/.gitconfig && echo .gitconfig hard linked) || echo .gitconfig hard link failed
 (ln -sf ~/.config/urxvt ~/.urxvt && echo .urxvt directory symbolically linked) || echo .urxvt symbolic link failed

0d7a4cc3c315bf4a10b3512d90d049a652545a67
Corey Williamson <nerd1001001@gmail.com>
2020-04-09 21:33:14 -0500

    Reimplemented directory shortener

--- ps1Getter.lua
+++ ps1Getter.lua
@@ -36,20 +36,36 @@ local time = os.date("%X")
 local user = os.getenv("USER")
 local host = sh("hostname")
 local wd = sh("pwd"):gsub("^/home/" .. user, "~")
-wd = wd .. (" "):rep(12 - #wd)
 local branch = sh("git branch 2> /dev/null | grep \\*")
 -- }}}
--- {{{ TODO: Directory shortener
+-- {{{ Directory shortener
+do
+	local dirmaxlen = 12
+	local dirstack = {}
+	for dir in wd:gmatch("[^/]+") do
+		tinsert(dirstack, #dir > dirmaxlen and dir:sub(1, dirmaxlen-3).."..." or dir)
+	end
+	local home = dirstack[1] == "~"
+	if #dirstack > 3 then
+		local idx = home and 2 or 1
+		repeat 
+			table.remove(dirstack, idx)
+		until #dirstack <= 3
+		table.insert(dirstack, idx, "...")
+	end
+	wd = (home and "" or "/") .. tconcat(dirstack, "/")
+	wd = wd .. (" "):rep(12-#wd)
+end
 -- }}}
-
+-- {{{ The Main Bits
 local entries = {}
-local escChar = string.char(27) .. "["
-local reset = escChar .. "0m"
+local escChar = "\\[" .. string.char(27) .. "["
+local reset = escChar .. "0m\\]"
 local function createEntry(str, color)
 	if str then
 		tinsert(entries, {
 			str = str,
-			color = escChar .. color .. "m",
+			color = escChar .. color .. "m\\]",
 		})
 	end
 end
@@ -67,7 +83,7 @@ for idx, entry in ipairs(entries) do
 	tinsert(result[2], entry.color .. entry.str .. reset)
 	tinsert(result[3], line.horiz:rep(utf8.len(entry.str)))
 end
-local lineColor = escChar .. "36m"
+local lineColor = escChar .. "36m\\]"
 -- fisrt line
 io.write("   ", lineColor, corner.tl, tconcat(result[1], t.down), corner.tr, "\n")
 -- second line
@@ -76,5 +92,5 @@ io.write(corner.tl, line.horiz:rep(2), t.left, tconcat(result[2], reset .. lineC
 io.write(line.horiz:rep(columns - middleLen), t.left, "\n")
 -- third line
 io.write(lineColor, line.vert, "  ", corner.bl, tconcat(result[3], t.up), corner.br, "\n")
-io.write(corner.bl, t.left, escChar .. "35m$ ", reset)
-
+io.write(lineColor, corner.bl, t.left, escChar .. "35m\\]$ ", reset)
+-- }}}

af87455c46725a756c345a0c593a23e4b2d7b465
Corey Williamson <euclidianAce@protonmail.com>
2020-08-27 04:55:18 -0500

    Dont comment empty lines

--- nvim/lua/commenter.lua
+++ nvim/lua/commenter.lua
@@ -6,7 +6,14 @@ local function escapeStr(str)
    return (str:gsub("[-+.*()%[%]]", "%%%1"))
 end
 
+local function trim(str)
+   return str:match("%s*(.*)%s*")
+end
+
 function M.commentStr(cs, str)
+   if trim(str) == "" then
+      return str
+   end
    local escc = escapeStr(cs)
    local patt = escc:gsub("%%s", "(.*)")
    local leadingWhitespace, line = str:match("(%s*)(.*)")

9fbd390bae14f860d3310e9c191f3ba505ff5415
Corey Williamson <euclidianAce@protonmail.com>
2020-09-24 00:54:10 -0500

    nvim luaprinter: use vim.loop to not block neovim when script goes wrong

--- nvim/lua/euclidian/luaprinter.lua
+++ nvim/lua/euclidian/luaprinter.lua
@@ -13,7 +13,7 @@ local function getLuaBuf(buf)
    if cache[buf] then
       return cache[buf]
    end
-   local ns = a.nvim_create_namespace("lua_stuffs")
+   local ns = a.nvim_create_namespace("luaprinter")
    return {
       buf = buf,
       ns = ns,
@@ -23,57 +23,66 @@ end
 
 
 
+
+local loop = vim.loop
 local function runBuffer(b)
-   local printed = setmetatable({}, { __index = function(self, key)
-         rawset(self, key, {})
-         return rawget(self, key)
-      end, })
-   local code = table.concat(a.nvim_buf_get_lines(b.buf, 0, -1, false), "\n")
-   local newPrint = function(...)
-      local line = debug.getinfo(2, "l").currentline
-      table.insert(
-printed[line],
-{ ... })
-
-   end
-   local ft = a.nvim_buf_get_option(b.buf, "ft")
-   local chunk, err
-   if ft == "teal" then
-      local tl = require("tl")
-      local env = tl.init_env(false, true)
-      chunk, err = tl.load(code, tostring(b.buf), "t", env)
-   else
-      chunk, err = loadstring(code)
-   end
-   if err then
-      print("Error:", err)
-   end
-   setfenv(chunk, setmetatable({}, {
-      __index = function(_, key)
-         if key == "print" then
-            return newPrint
-         end
-         return _G[key]
-      end,
-   }))
-   local ok, res = pcall(chunk)
-   if not ok then
-      print("Error:", res)
-   end
-   a.nvim_buf_clear_namespace(b.buf, b.ns, 0, -1)
-
-   local offset = ft == "teal" and -1 or 1
-   for linenum, data in pairs(printed) do
-      local text = {}
-      for _, arr in ipairs(data) do
-         for _, v in ipairs(arr) do
-            table.insert(text, { vim.inspect(v), "Comment" })
-            table.insert(text, { " ", "Comment" })
-         end
-         table.insert(text, { "  ", "Comment" })
+   local info = {}
+   local function onread(err, data)
+      if err then
+         error(err)
       end
-      a.nvim_buf_set_virtual_text(b.buf, b.ns, linenum - offset, text, {})
+      if data then
+         for lnum, str in data:gmatch(string.char(1) .. "(%d+)" .. string.char(1) .. "(.-)" .. string.char(1)) do
+            local lineNum = tonumber(lnum)
+            if not info[lineNum] then
+               info[lineNum] = {}
+            end
+            table.insert(info[lineNum], str)
+         end
+      end
    end
+
+   local stdout = loop.new_pipe(false)
+   local stderr = loop.new_pipe(false)
+   local handle
+
+   local closed = false
+   local close = vim.schedule_wrap(function()
+      if not closed then
+         closed = true
+         stdout:read_stop()
+         stderr:read_stop()
+         stdout:close()
+         stderr:close()
+         handle:close()
+         a.nvim_buf_clear_namespace(b.buf, b.ns, 0, -1)
+         for linenum, data in pairs(info) do
+            a.nvim_buf_set_virtual_text(b.buf, b.ns, linenum - 1, { { table.concat(data, "  "), "Comment" } }, {})
+         end
+      end
+   end)
+   local name = a.nvim_buf_get_name(b.buf)
+   handle = loop.spawn("lua", {
+      args = {
+         '-l', 'inspect',
+         '-e', [[print = function(...)
+				io.stdout:write(string.char(1), debug.getinfo(2, "l").currentline, string.char(1))
+				for i = 1, select("#", ...) do
+					io.stdout:write(inspect((select(i, ...))))
+					if i < select("#", ...) then
+						io.stdout:write(", ")
+					end
+				end
+				io.stdout:write(string.char(1))
+			end]],
+         name,
+      },
+      stdio = { stdout, stderr },
+   }, close)
+   loop.read_start(stdout, onread)
+   loop.read_start(stderr, onread)
+
+   vim.defer_fn(close, 10 * 1000)
 end
 
 function M.runBuffer(buf)

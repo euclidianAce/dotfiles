8d43ce7df79e10214ace015edd7d8dc832233bcd
Corey Williamson <euclidianAce@protonmail.com>
2020-11-04 00:07:27 -0600

    Add ft snippets

--- nvim/lua/euclidian/snippet.lua
+++ nvim/lua/euclidian/snippet.lua
@@ -37,6 +37,13 @@ local Evaluator = {BufWinInfo = {}, }
 
 
 local snippets = {}
+local ftSnippets = setmetatable({}, {
+   __index = function(self, key)
+      local t = {}
+      rawset(self, key, t)
+      return t
+   end,
+})
 local evaluators = {}
 
 local function createEvaluator(kind)
@@ -48,17 +55,14 @@ local function createEvaluator(kind)
    local outputWin = a.nvim_open_win(outputBuf, false, {
       relative = "cursor", style = "minimal", anchor = "NW",
       width = 50, height = 5,
-      row = -2, col = -4,
+      row = -7, col = -4,
    })
    local inputBuf = a.nvim_create_buf(false, true)
    local inputWin = a.nvim_open_win(inputBuf, true, {
       relative = "cursor", style = "minimal", anchor = "NW",
       width = 50, height = 5,
-      row = 1, col = -4,
+      row = 3, col = -4,
    })
-
-
-
 
    a.nvim_win_set_option(outputWin, "winhl", "NormalFloat:Special")
    local e = {
@@ -70,8 +74,8 @@ local function createEvaluator(kind)
    }
    evaluators[origWin] = e
    cmdf([[autocmd TextChanged,TextChangedI <buffer=%d> lua require'euclidian.snippet'.eval(%d)]], e.input.buf, e.orig.win)
-   cmdf([[inoremap <silent> <buffer> <CR> <Esc>:lua require'euclidian.snippet'.step(%d)<CR>]], e.orig.win)
-   cmdf([[nnoremap <silent> <buffer> <CR> <Esc>:lua require'euclidian.snippet'.step(%d)<CR>]], e.orig.win)
+   cmdf([[inoremap <silent> <buffer> <CR> <cmd>lua require'euclidian.snippet'.step(%d)<CR>]], e.orig.win)
+   cmdf([[nnoremap <silent> <buffer> <CR> <cmd>lua require'euclidian.snippet'.step(%d)<CR>]], e.orig.win)
    cmdf("startinsert")
    return e
 end
@@ -86,7 +90,11 @@ vim.split(
       if not n then
          return m
       elseif n - 1 == #e.snippetInput then
-         return a.nvim_buf_get_lines(e.input.buf, 0, -1, false)[1]
+         local res = a.nvim_buf_get_lines(e.input.buf, 0, -1, false)[1]
+         return res ~= "" and
+         res or
+         e.snippet.defaults[n] or
+         ""
       else
          return e.snippetInput[n]
       end
@@ -102,18 +110,34 @@ local function delete(e)
    evaluators[e.orig.win] = nil
 end
 
+local function putResult(e)
+   local result = a.nvim_buf_get_lines(e.output.buf, 0, -1, false)
+   a.nvim_buf_set_lines(e.orig.buf, e.cursorRow - 1, e.cursorRow - 1, false, result)
+   a.nvim_win_set_cursor(e.orig.win, { e.cursorRow, 0 })
+   delete(e)
+   a.nvim_input(string.format("<Esc>%d==j", #result))
+end
+
 
 local stub = function() end
 local function evaluate(e)
    if e.kind == "start" then
+      local current_ft = a.nvim_buf_get_option(e.orig.buf, "ft")
       local name = a.nvim_buf_get_lines(e.input.buf, 0, -1, false)[1]
-      if snippets[name] then
-         e.kind = "snippet"
-         e.snippetStep = 1
-         e.snippetInput = {}
-         e.snippet = snippets[name]
-         a.nvim_buf_set_lines(e.input.buf, 0, -1, false, {})
-         a.nvim_buf_set_lines(e.output.buf, 0, -1, false, vim.split(snippets[name].content, "\n"))
+
+      local snip = ftSnippets[current_ft][name] or snippets[name]
+
+      if snip then
+         a.nvim_buf_set_lines(e.output.buf, 0, -1, false, vim.split(snip.content, "\n"))
+         if snip.length == 0 then
+            putResult(e)
+         else
+            e.kind = "snippet"
+            e.snippetStep = 1
+            e.snippetInput = {}
+            e.snippet = snip
+            a.nvim_buf_set_lines(e.input.buf, 0, -1, false, {})
+         end
       elseif name == "lua" then
          e.kind = "lua"
          a.nvim_buf_set_lines(e.input.buf, 0, -1, false, {})
@@ -130,27 +154,24 @@ local function evaluate(e)
    end
 end
 
-local function putResult(e)
-   local result = a.nvim_buf_get_lines(e.output.buf, 0, -1, false)
-   delete(e)
-   a.nvim_put(result, "l", true, true)
-end
-
 local function step(e)
    local content = a.nvim_buf_get_lines(e.input.buf, 0, -1, false)[1]
    if e.kind == "snippet" then
-      table.insert(e.snippetInput, content)
-      if e.snippetStep == e.snippet.length then
-         putResult(e)
-         evaluators[e.orig.win] = nil
+      if content == "" then
+         content = e.snippet.defaults[#e.snippetInput + 1] or content
       end
-      a.nvim_buf_set_lines(e.input.buf, 0, -1, false, {})
-      updateEvalBuf(e)
-      e.snippetStep = e.snippetStep + 1
-      cmdf("startinsert")
+      table.insert(e.snippetInput, content)
+      if e.snippetStep >= e.snippet.length then
+         updateEvalBuf(e)
+         putResult(e)
+      else
+         a.nvim_buf_set_lines(e.input.buf, 0, -1, false, {})
+         updateEvalBuf(e)
+         e.snippetStep = e.snippetStep + 1
+         cmdf("startinsert")
+      end
    elseif e.kind == "lua" then
       putResult(e)
-      evaluators[e.orig.win] = nil
    end
 end
 
@@ -160,8 +181,23 @@ function snippet.create(name, content, defaults)
    snippets[name] = {
       content = content,
       defaults = defaults or {},
+      length = select(2, content:gsub("%%%d+", "%1")),
    }
-   snippets[name].length = select(2, content:gsub("%%%d+", "%1"))
+end
+
+function snippet.ftCreate(ft, name, content, defaults)
+   local snip = {
+      content = content,
+      defaults = defaults or {},
+      length = select(2, content:gsub("%%%d+", "%1")),
+   }
+   if type(ft) == "string" then
+      ftSnippets[ft][name] = snip
+   else
+      for i, v in ipairs(ft) do
+         ftSnippets[v][name] = snip
+      end
+   end
 end
 
 function snippet.start()

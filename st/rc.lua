68b97174bb97df0f1e8ac6de63085b2ae590e226
Corey Williamson <euclidianAce@protonmail.com>
2021-03-03 12:38:28 -0600

    add st

---
+++ st/rc.lua
@@ -0,0 +1,40 @@
+
+local _ENV = st
+
+font = "terminus:size=12"
+-- font = "Fixedsys Excelsior:size=12"
+-- font = "Ubuntu Mono:size=12:antialias=true"
+-- font = "FiraCode Light:size=12:antialias=true"
+
+cwscale = 1
+chscale = 1
+
+colorname = {
+	-- 8 normal colors
+	"#181520",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
+
+	-- 8 bright colors
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
+}
+
+defaultfg = 7
+defaultbg = 0
+
+-- TODO: implement this
+-- shortcuts = {
+	-- { xk.any_mod, xb.break, ttysend }
+-- }

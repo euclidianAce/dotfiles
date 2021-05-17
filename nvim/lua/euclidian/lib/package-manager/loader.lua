a9b6d491149fb90f55fdf3b077a2e62f5704b30b
Corey Williamson <euclidianAce@protonmail.com>
2021-05-17 10:25:42 -0500

    load packages in correct order

--- nvim/lua/euclidian/lib/package-manager/loader.lua
+++ nvim/lua/euclidian/lib/package-manager/loader.lua
@@ -1,6 +1,9 @@
 
+local packagespec = require("euclidian.lib.package-manager.packagespec")
 local set = require("euclidian.lib.package-manager.set")
 local loader = {}
+
+local Spec = packagespec.Spec
 
 
 
@@ -14,9 +17,49 @@ local function packadd(pkg)
    command(([[packadd %s]]):format(pkg))
 end
 
+local function getLoadOrder(loadedSet)
+   local stages = setmetatable({}, {
+      __index = function(self, key)
+         rawset(self, key, {})
+         return rawget(self, key)
+      end,
+   })
+
+   for _, p in ipairs(loadedSet) do
+      stages[1][p] = true
+   end
+
+   local idx = 1
+   local maxLen = #loadedSet + 1
+   repeat
+      assert(idx <= maxLen, "circular dependency detected")
+      local done = true
+
+      for p in pairs(stages[idx]) do
+         if p.dependents then
+            done = false
+            for _, dep in ipairs(p.dependents) do
+               stages[idx][dep] = nil
+               stages[idx + 1][dep] = true
+            end
+         end
+      end
+      idx = idx + 1
+   until done
+
+   local order = {}
+   for _, stage in ipairs(stages) do
+      for p in pairs(stage) do
+         table.insert(order, p)
+      end
+   end
+
+   return order
+end
+
 local function packaddSet(setname)
    local pre, post = {}, {}
-   local ps = set.load(setname)
+   local ps = getLoadOrder(set.load(setname))
    for _, pkg in ipairs(ps) do
       if pkg:isInstalled() then
          if pkg.kind == "git" then

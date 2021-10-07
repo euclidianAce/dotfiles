5c739fcd0973dd10913a6763583aa2f48d1fc215
Corey Williamson <euclidianAce@protonmail.com>
2021-10-06 20:06:32 -0500

    font

--- st/rc.lua
+++ st/rc.lua
@@ -3,8 +3,8 @@
 -- st.font = "FiraCode Light:size=12:antialias=true"
 -- st.font = "Fixedsys Excelsior:size=12:antialias=false:bold=false" -- this has problems with width
 -- st.font = "Hasklig:size=14:bold=false:antialias=true"
--- st.font = "Ubuntu Mono:size=12:antialias=true"
-st.font = "terminus:size=12:antialias=true"
+st.font = "Ubuntu Mono:size=12:antialias=true"
+-- st.font = "terminus:size=12:antialias=true"
 -- st.font = "Julia Mono:size=12:antialias=true"
 -- st.font = "CozetteVector:size=11:antialias=false"
 -- st.font = "cozette:size=11:antialias=true"

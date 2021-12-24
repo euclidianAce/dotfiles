01eb7b667d2c406a2edeb12e3858f6509cd81813
Corey Williamson <euclidianAce@protonmail.com>
2021-12-23 20:21:41 -0600

    locationjump: handle multiple locations

--- nvim/lua/euclidian/plug/locationjump/api.lua
+++ nvim/lua/euclidian/plug/locationjump/api.lua
@@ -2,18 +2,7 @@ local nvim = require("euclidian.lib.nvim")
 
 local locationjump = {}
 
-local pattern = "^(.*):(%d+)$"
-
-function locationjump.setPattern(p)
-   pattern = p
-end
-
-
-function locationjump.parseLocation(loc)
-   assert(loc)
-   local file, line = loc:match(pattern)
-   return file, tonumber(line)
-end
+local pattern = "([^%s:]+):(%d+)"
 
 function locationjump.jump(filename, line)
    if line then
@@ -23,13 +12,52 @@ function locationjump.jump(filename, line)
    end
 end
 
-function locationjump.parseAndJump(loc)
-   local file, line = locationjump.parseLocation(loc)
-   if file and line then
-      nvim.command("edit +%d %s", line, file)
+function locationjump.selectLocation(locations)
+   if #locations > 1 then
+      vim.ui.select(locations, {
+         prompt = "Multiple locations found:",
+         format_item = function(item)
+            if item[2] then
+               return ("%s @ line %d"):format(item[1], item[2])
+            end
+            return ("%s"):format(item[1])
+         end,
+      }, function(item)
+         if item then
+            locationjump.jump(item[1], item[2])
+         end
+      end)
    else
-      vim.api.nvim_err_writeln("locationjump: Unable to parse file location from '" .. loc .. "'")
+      locationjump.jump(locations[1][1], locations[1][2])
    end
+end
+
+function locationjump.setPattern(p)
+   pattern = p
+end
+
+function locationjump.parseAllLocations(str)
+   local results = {}
+   for a, b in str:gmatch(pattern) do
+      local n = tonumber(b)
+      table.insert(results, { a, n })
+   end
+   if #results == 0 then
+      vim.api.nvim_err_writeln("locationjump: Unable to parse file location from '" .. str .. "'")
+   end
+   return results
+end
+
+
+function locationjump.parseLocation(loc)
+   assert(loc)
+   local file, line = loc:match("^" .. pattern .. "$")
+   return file, tonumber(line)
+end
+
+function locationjump.parseAndJump(text)
+   local results = locationjump.parseAllLocations(text)
+   locationjump.selectLocation(results)
 end
 
 function locationjump.jumpToVisualSelection()
@@ -38,11 +66,10 @@ function locationjump.jumpToVisualSelection()
    local a = buf:getMark("<")
    local b = buf:getMark(">")
    local lines = buf:getLines(a[1] - 1, b[1], true)
-   if #lines ~= 1 then
-      return
-   end
-   local text = lines[1]:sub(a[2] + 1, b[2] + 1)
-   locationjump.parseAndJump(text)
+   local text = table.concat(lines, "\n")
+
+   local results = locationjump.parseAllLocations(text)
+   locationjump.selectLocation(results)
 end
 
 function locationjump.jumpExpand(expandArg)

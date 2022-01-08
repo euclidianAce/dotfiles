a8ed3602798935fcab7d9fba9bcb15333ccb27b3
Corey Williamson <euclidianAce@protonmail.com>
2022-01-07 22:25:24 -0600

    Reword validator lib
    still don't use it anywhere, but it's got cool ideas

--- nvim/lua/euclidian/lib/validate.lua
+++ nvim/lua/euclidian/lib/validate.lua
@@ -6,66 +6,155 @@ local Validator = {}
 
 
 
+
+
+
+
+
+
+
+
 local validatorMt = {
    __index = Validator,
 }
 
-local function new(checker)
-   return setmetatable({ _checker = checker }, validatorMt)
-end
-
-function validatorMt.__add(self, other)
-   local a = self._checker
-   local b = other._checker
-   return new(function(val)
-      do
-         local ok, err = a(val)
-         if not ok then
-            return false, err
-         end
-      end
-      do
-         local ok, err = b(val)
-         if not ok then
-            return false, err
-         end
-      end
-      return true
-   end)
-end
-
-function validatorMt.__call(self, val)
-   return self._checker(val)
+local function new(checker, desc)
+   return setmetatable({ checker = assert(checker), desc = assert(desc) }, validatorMt)
 end
 
 local validate = {
    Validator = Validator,
 
    new = new,
+   inspector = tostring,
+
+   toNumber = new(
+   function(val)
+      return tonumber(val) ~= nil
+   end,
+   "be convertible to a number"),
+
 }
+
+local function inspect(value)
+   return ((validate.inspector or tostring)(value))
+end
+
+
+function validatorMt.__add(self, other)
+   local a = self.checker
+   local b = other.checker
+   return new(
+   function(val)
+      return a(val) or b(val)
+   end,
+   self.desc .. " or " .. other.desc)
+
+end
+
+
+
+function validatorMt.__sub(self, other)
+   local a = self.checker
+   local b = other.checker
+   return new(
+   function(val)
+      return a(val) or not b(val)
+   end,
+   self.desc .. " or not " .. other.desc)
+
+end
+
+
+function validatorMt.__mul(self, other)
+   local a = self.checker
+   local b = other.checker
+   return new(
+   function(val)
+      return a(val) and b(val)
+   end,
+   self.desc .. " and " .. other.desc)
+
+end
+
+
+function validatorMt.__div(self, other)
+   local a = self.checker
+   local b = other.checker
+   return new(
+   function(val)
+      return a(val) and not b(val)
+   end,
+   self.desc .. " and not " .. other.desc)
+
+end
+
+
+function validatorMt.__unm(self)
+   local a = self.checker
+   return new(
+   function(val)
+      return not a(val)
+   end,
+   "not " .. self.desc)
+
+end
+
+function validatorMt.__call(self, val)
+   local ok = self.checker(val)
+   if not ok then
+      return false, ("expected <%s> to %s"):format(inspect(val), self.desc)
+   end
+   return true
+end
 
 local typeValidators = setmetatable({}, { __mode = "k" })
 function validate.type(t)
    if not typeValidators[t] then
-      typeValidators[t] = new(function(val)
-         if type(val) ~= t then
-            return false, "value is not a " .. t
-         end
-         return true
-      end)
+      typeValidators[t] = new(
+      function(val)
+         return type(val) == t
+      end,
+      "be of type " .. t)
+
    end
    return typeValidators[t]
 end
 
-function validate.range(a, b)
-   return validate.type("number") + new(function(val)
+function validate.range(a, b, fmt)
+   fmt = fmt or "%f"
+   return validate.type("number") *
+   new(
+   function(val)
       assert(type(val) == "number")
-      if not (a <= val and
-         val <= b) then
-         return false, ("%s is not within [%f, %f]"):format(tostring(val), a, b)
+      return (a <= val and
+      val <= b)
+   end,
+   ("be within [" .. fmt .. ", " .. fmt .. "]"):format(a, b))
+
+end
+
+function validate.hasKey(name, maybe_validator)
+   local val_cond = maybe_validator and maybe_validator.checker
+   local desc = "have key " .. name
+   if maybe_validator then
+      desc = desc .. " and that key to " .. maybe_validator.desc
+   end
+   return new(
+   function(val)
+      local ok, res = pcall(function()
+         return (val)[name]
+      end)
+      if not ok or rawequal(res, nil) then
+         return false
+      end
+      if val_cond then
+         return val_cond(res)
       end
       return true
-   end)
+   end,
+   desc)
+
 end
 
 return validate

e2105a5c4c6df91dafac6973f127d5ee33dff855
Corey Williamson <euclidianAce@protonmail.com>
2022-02-02 20:45:44 -0600

    color lib: add hsv stuff

--- nvim/lua/euclidian/lib/color.lua
+++ nvim/lua/euclidian/lib/color.lua
@@ -86,4 +86,62 @@ function color.rgbToHex(r, g, b)
    return bit32.bor(bit32.bor((bit32.lshift(r, 16)), (bit32.lshift(g, 8))), b)
 end
 
+function color.rgbToHsv(r, g, b)
+   local rs = r / 255
+   local gs = g / 255
+   local bs = b / 255
+   local mmax = math.max(rs, gs, bs)
+   local mmin = math.min(rs, gs, bs)
+   local delta = mmax - mmin
+
+   local h
+   if delta > 0 then
+      if mmax == rs then
+         h = (gs - bs) / delta
+      elseif mmax == gs then
+         h = (bs - rs) / delta + 2
+      elseif mmax == bs then
+         h = (rs - gs) / delta + 4
+      end
+   end
+
+   local v = mmax
+   local s = v == 0 and 0 or delta / v
+
+   return h, s, v
+end
+
+
+
+
+function color.hsvToRgb(h, s, v)
+   local alpha = v * (1 - s)
+   local beta = h and v * (1 - (h - math.floor(h)) * s)
+   local gamma = h and v * (1 - (1 - (h - math.floor(h))) * s)
+
+   if not h then
+      return v, v, v
+   end
+   h = h % 6
+
+   assert(alpha)
+   assert(beta)
+   assert(gamma)
+
+   if h < 1 then
+      return v, gamma, alpha
+   elseif h < 2 then
+      return beta, v, alpha
+   elseif h < 3 then
+      return alpha, v, gamma
+   elseif h < 4 then
+      return alpha, beta, v
+   elseif h < 5 then
+      return gamma, alpha, v
+   elseif h < 6 then
+      return v, alpha, beta
+   end
+   error("bad h value: h=" .. tostring(h))
+end
+
 return color

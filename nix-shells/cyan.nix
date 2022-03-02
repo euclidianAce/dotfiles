4b54260ad674960cb2ac59b541b788ded3125ccc
Corey Williamson <euclidianAce@protonmail.com>
2022-03-02 01:02:28 -0600

    nix-shells/cyan: add inspect

--- nix-shells/cyan.nix
+++ nix-shells/cyan.nix
@@ -1,14 +1,27 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
+    # TODO: make this configurable?
+    # lua5_1
+    # lua51Packages.argparse
+    # lua51Packages.luafilesystem
+    # lua51Packages.busted
+    # lua51Packages.inspect
+    # lua51Packages.compat53
+
     lua5_3
     lua53Packages.argparse
     lua53Packages.luafilesystem
     lua53Packages.busted
+    lua53Packages.inspect
   ];
   shellHook = ''
     append-to-path () {
       export PATH="$PATH:$1"
+    }
+
+    prepend-to-lua-path () {
+      export LUA_PATH="$1/?.lua;$1/?/init.lua;$LUA_PATH"
     }
 
     append-to-lua-path () {
@@ -26,8 +39,13 @@ pkgs.mkShell {
     append-to-path "$HOME/dev/tl"
     append-to-lua-path "$HOME/dev/tl"
 
+    append-to-lua-path "$HOME/dev/ltreesitter"
+    append-to-lua-cpath "$HOME/dev/ltreesitter"
+    append-to-lua-cpath "$HOME/dev/tree-sitter-teal"
     append-to-path "$HOME/dev/cyan/bin"
     append-to-lua-path "$HOME/dev/cyan/build"
     append-to-lua-path "$HOME/dev/cyan/src"
+
+    prepend-to-lua-path "./"
   '';
 }

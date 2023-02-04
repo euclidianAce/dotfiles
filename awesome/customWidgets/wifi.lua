a057ea3186f3e7a2cd167c2725b17330def62735
Corey Williamson <euclidianAce@protonmail.com>
2023-02-03 23:42:47 -0600

    awesome: clean up wifi widget a little

--- awesome/customWidgets/wifi.lua
+++ awesome/customWidgets/wifi.lua
@@ -14,74 +14,98 @@ local cmd = wpafmt:format("list_networks | grep CURRENT")
 local badText = " Not Connected "
 local text = wibox.widget.textbox(badText)
 
-local menu
 local menuitems = {}
 
-local fh = io.open(config.wpaSupplicantConfig or "/etc/wpa_supplicant.conf", "r")
-if fh then
-	local idx = -1
-	local function nextLine()
-		while true do
-			local ln = fh:read("*l")
-			if not ln then return end
-			local networkName = ln:match("%s*ssid=\"(.*)\"%s*$")
-			if networkName then
-				idx = idx + 1
-				return networkName, idx
-			end
-		end
+local function readAllSync(filename)
+	local fh, err = io.open(filename, "r")
+	if not fh then
+		return nil, err
 	end
-	for name, num in nextLine do
+	local contents = fh:read("*a")
+	fh:close()
+	return contents
+end
+
+local toTry = {
+	"/etc/wpa_supplicant.conf",
+	config.wpaSupplicantConfig,
+}
+
+local configContents
+do
+	local errs = {}
+	for _, v in ipairs(toTry) do
+		local err
+		configContents, err = readAllSync(v)
+		if configContents then
+			break
+		end
+		table.insert(errs, v .. ": " .. err)
+	end
+
+	if not configContents then
+		naughty.notify {
+			preset = naughty.config.presets.critical,
+			title = "Wifi Widget Error",
+			text = table.concat(errs, "\n"),
+		}
+		return wibox.widget.textbox("???")
+	end
+end
+
+local zero_index = -1
+for line in configContents:gmatch("[^\n]") do
+	local networkName = line:match("%s*ssid=\"(.*)\"%s*$")
+	if networkName then
+		zero_index = zero_index + 1
 		table.insert(menuitems, {
 			name,
 			function()
-				naughty.notify{
-					title = "selected:",
-					text = name, --wpafmt:format("select_network " .. num)
-				}
-
-				-- TODO: im like 90% sure theres a way to asynchronously spawn a shell with awesome,
-				-- but this shouldn't block for like any amount of time so :P
-				os.execute(wpafmt:format("select_network " .. num))
+				naughty.notify{ text = "selected network: " .. name }
+				awful.spawn.easy_async(wpafmt:format("select_network " .. num))
 			end
 		})
 	end
-	fh:close()
-	menu = awful.menu{
-		items = menuitems,
-	}
 end
 
+local menu = awful.menu{
+	items = menuitems,
+}
 
 local wifi = wibox.widget {
 	layout = wibox.layout.align.horizontal,
-	{widget = awful.widget.watch(("sh -c '%s'"):format(cmd), 5, function(widget, stdout)
-		local start = stdout:find("\t")
-		if not start then
-			widget:set_text(badText)
-			return
-		else
-			start = start + 1
-		end
+	{
+		widget = awful.widget.watch(
+			("sh -c '%s'"):format(cmd),
+			5,
+			function(widget, stdout)
+				local start = stdout:find("\t")
+				if not start then
+					widget:set_text(badText)
+					return
+				else
+					start = start + 1
+				end
 
-		local finish = stdout:find("\t", start)
-		if not finish then
-			widget:set_text(badText)
-			return
-		else
-			finish = finish - 1
-		end
+				local finish = stdout:find("\t", start)
+				if not finish then
+					widget:set_text(badText)
+					return
+				else
+					finish = finish - 1
+				end
 
-		widget:set_text(stdout:sub(start, finish))
-	end, text)}
+				widget:set_text(stdout:sub(start, finish))
+			end,
+			text
+		)
+	}
 }
 
-if menu then
-	wifi:buttons(gears.table.join(
-		awful.button({}, 1, function()
-			menu:toggle()
-		end)
-	))
-end
+wifi:buttons(gears.table.join(
+	awful.button({}, 1, function()
+		menu:toggle()
+	end)
+))
 
 return wifi

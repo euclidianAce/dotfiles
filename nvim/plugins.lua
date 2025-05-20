68b789742a4c09badffab1731619fd2a4343d4d8
Corey Williamson <euclidianAce@protonmail.com>
2025-05-19 22:17:54 -0500

    nvim: add just plugin

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -2,6 +2,7 @@
 plugin { git = "https://tpope.io/vim/fugitive.git" }
 plugin { tar = { url = "https://github.com/neovim/nvim-lspconfig/archive/refs/tags/v1.6.0.tar.gz", name = "lspconfig" } }
 plugin { git = "https://github.com/kaarmu/typst.vim.git" }
+plugin { git = "https://github.com/NoahTheDuke/vim-just.git" }
 
 local fzf_share_dir = vim.fn.system("fzf-share"):sub(1, -2)
 if vim.v.shell_error == 0 then

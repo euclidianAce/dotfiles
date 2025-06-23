469c6de23aa3926a4d47e5b0cdacdfcc4a3ce1d0
Corey Williamson <euclidianAce@protonmail.com>
2025-06-23 13:55:14 -0500

    colors: generate wezterm config colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,45 +2,30 @@ local wezterm = require "wezterm"
 
 local config = wezterm.config_builder and wezterm.config_builder() or {}
 
+do
+	config.colors = {}
+	local ok, color_entries = pcall(require, "color")
+	if ok then
+		for k, v in pairs(color_entries) do
+			config.colors[k] = v
+		end
+	end
+end
+
 -- config.font = wezterm.font "CozetteVector"
 -- config.font_size = 8
 
 -- config.font = wezterm.font "UbuntuMono"
--- config.font_size = 10
+-- config.font_size = 12
 
-config.font = wezterm.font "Terminus (TTF)"
-config.font_size = 9
+-- config.font = wezterm.font "Terminus (TTF)"
+-- config.font_size = 9
 
--- config.font = wezterm.font "Atkinson Hyperlegible Mono"
+config.font = wezterm.font "Atkinson Hyperlegible Mono"
+config.font_size = 12
 
 -- Works around a rendering bug
 config.front_end = "WebGpu"
-
-config.colors = {
-	foreground = "#d8cee4",
-	background = "#181520",
-
-	ansi = {
-		"#464252",
-		"#d16161",
-		"#53b67e",
-		"#d5c876",
-		"#799ae0",
-		"#c24472",
-		"#429da0",
-		"#817998",
-	},
-	brights = {
-		"#817998",
-		"#e69090",
-		"#7bce8f",
-		"#f0e7ac",
-		"#aac3fd",
-		"#ef6194",
-		"#70c3c6",
-		"#efefef",
-	}
-}
 
 config.hide_tab_bar_if_only_one_tab = true
 
